f7896b299568333a23f569fb2bff6f4458289ce3
cesarlopezfa <cesarlopezfa@gmail.com>
2016-07-02 00:25:11 +0200

    Add hierarchical context runner moving code around

--- src/test/java/es/cesarlopezfab/starter/CellTest.java
+++ src/test/java/es/cesarlopezfab/starter/CellTest.java
@@ -4,58 +4,66 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
+
+@RunWith(HierarchicalContextRunner.class)
 public class CellTest {
 
-	@Test
-	public void createsEmptyCell() {
-		Cell cell = CellFactory.empty();
-		
-		assertTrue(cell.isEmpty());
+	public class Content {
+		@Test
+		public void createsEmptyCell() {
+			Cell cell = CellFactory.empty();
+
+			assertTrue(cell.isEmpty());
+		}
 	}
-	
-	@Test
-	public void connectsNorthSouthTwoEmptyCells() {
-		Cell north = CellFactory.empty();
-		Cell south = CellFactory.empty();
-		
-		north.connectSouth(south);
-		
-		assertEquals(south, north.south());
-		assertEquals(north, south.north());
+
+	public class Connection {
+		@Test
+		public void connectsNorthSouthTwoEmptyCells() {
+			Cell north = CellFactory.empty();
+			Cell south = CellFactory.empty();
+
+			north.connectSouth(south);
+
+			assertEquals(south, north.south());
+			assertEquals(north, south.north());
+		}
+
+		@Test
+		public void connectsSouthNorthTwoEmptyCells() {
+			Cell north = CellFactory.empty();
+			Cell south = CellFactory.empty();
+
+			south.connectNorth(north);
+
+			assertEquals(south, north.south());
+			assertEquals(north, south.north());
+		}
+
+		@Test
+		public void connectsEastWestTwoEmptyCells() {
+			Cell east = CellFactory.empty();
+			Cell west = CellFactory.empty();
+
+			east.connectWest(west);
+
+			assertEquals(west, east.west());
+			assertEquals(east, west.east());
+		}
+
+		@Test
+		public void connectsWestEastTwoEmptyCells() {
+			Cell west = CellFactory.empty();
+			Cell east = CellFactory.empty();
+
+			west.connectEast(east);
+
+			assertEquals(east, west.east());
+			assertEquals(west, east.west());
+		}
 	}
-	
-	@Test
-	public void connectsSouthNorthTwoEmptyCells() {
-		Cell north = CellFactory.empty();
-		Cell south = CellFactory.empty();
-		
-		south.connectNorth(north);
-		
-		assertEquals(south, north.south());
-		assertEquals(north, south.north());
-	}
-	
-	@Test
-	public void connectsEastWestTwoEmptyCells() {
-		Cell east = CellFactory.empty();
-		Cell west = CellFactory.empty();
-		
-		east.connectWest(west);
-		
-		assertEquals(west, east.west());
-		assertEquals(east, west.east());
-	}
-	
-	@Test
-	public void connectsWestEastTwoEmptyCells() {
-		Cell west = CellFactory.empty();
-		Cell east = CellFactory.empty();
-		
-		west.connectEast(east);
-		
-		assertEquals(east, west.east());
-		assertEquals(west, east.west());
-	}
-	
+
 }
